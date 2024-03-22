0372729e8aa11db2379511f91eb188a33d2fa983
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-22 12:49:20 +0530

    Update all.py

--- plugins/all.py
+++ plugins/all.py
@@ -2,10 +2,12 @@ from pyrogram import Client, filters
 import re
 
 # Define the user ID of the specific user whose messages you want to forward
-specific_user_id = "6317295439"
+specific_user_id = 6317295439
+
+source_chat_id = -1001932992146
 
 # Define the target chats where you want to forward the messages
-target_chats = ["-1001726137386", "-1001870457773"]
+target_chats = [-1001726137386, -1001870457773]
 
 # Function to check if a message contains any URLs
 def contains_url(text):
@@ -14,9 +16,9 @@ def contains_url(text):
     return re.search(url_regex, text) is not None
 
 # Forward messages from the specific user if they contain URLs to the target chats
-@Client.on_message(filters.user(specific_user_id) & ~filters.forwarded)
+@Client.on_message(filters.chat(source_chat_id) & filters.user(specific_user_id) & ~filters.forwarded)
 async def forward_message(client, message):
-    if contains_url(message.text):
+    if message.text and contains_url(message.text):
         for chat_id in target_chats:
             await client.forward_messages(
                 chat_id=chat_id, 
