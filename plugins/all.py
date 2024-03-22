12fbec018337d043ba678c3186a0eb69739a1840
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-22 20:40:41 +0530

    Update all.py

--- plugins/all.py
+++ plugins/all.py
@@ -1,27 +1,28 @@
 from pyrogram import Client, filters
 import re
 
-SOURCE_USER_ID = 6317295439
+SOURCE_USER = "https://t.me/ekconverter9bot"
 SOURCE_CHAT_ID = -1001932992146
-TARGET_CHAT_ID = [-1001726137386, -1001870457773]
+TARGET_CHAT_IDS = [-1001726137386, -1001870457773]
 
 # Function to check if a message contains any URLs
 def contains_url(text):
     # Regular expression to match URLs
-    url_regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url_regex = r"(https)"
     return re.search(url_regex, text) is not None
 
 
-
 # Forward messages from the specific user if they contain URLs to the target chats
-@Client.on_message(filters.group & filters.text & filters.incoming & filters.user(SOURCE_USER_ID))
+@Client.on_message(filters.group & filters.text & filters.incoming))
 async def forward_message(client, message):
     if message.chat.id != SOURCE_CHAT_ID:
-        if message.text:
-            for chat_id in TARGET_CHAT_ID:
-                await client.send_message(
-                    chat_id=chat_id, 
-                    text=message.text
-                )
-    
-    
+        content = message.text
+        if contains_url(content):
+            user = message.from_user.mention
+            if user == SOURCE_USER:
+                for chat_id in TARGET_CHAT_IDS:
+                    await client.send_message(
+                        chat_id=chat_id, 
+                        text=message.text
+                    )
+
