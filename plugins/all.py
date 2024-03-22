fcb25f0246a2a6ce2b66ee74260a4eb0f225fd58
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-22 22:46:18 +0530

    Update all.py

--- plugins/all.py
+++ plugins/all.py
@@ -3,15 +3,15 @@ from info import AUTH_CHANNEL
 
 # Forward messages from the specific user if they contain URLs to the target chats
 @Client.on_message(filters.group & filters.text & filters.incoming)
-async def forward_message(client, message):
+async def forward_message(bot, message):
     content = message.text
     if message.reply_markup is None:
-        await client.send_message(
+        await bot.send_message(
             chat_id=AUTH_CHANNEL, 
             text=message.text
         )
     else:
-        await client.send_message(
+        await bot.send_message(
             chat_id=AUTH_CHANNEL,
             text=f"⚡ {message.text}"
         )
