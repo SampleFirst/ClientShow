00df279bd0cde723cb8866b777a3bbfb828a37ad
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-22 22:37:12 +0530

    Update all.py

--- plugins/all.py
+++ plugins/all.py
@@ -1,13 +1,17 @@
 from pyrogram import Client, filters
 from info import AUTH_CHANNEL
 
-
 # Forward messages from the specific user if they contain URLs to the target chats
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def forward_message(client, message):
     content = message.text
-    await client.send_message(
-        chat_id=AUTH_CHANNEL, 
-        text=message.text
-    )
-
+    if message.reply_markup is None:
+        await client.send_message(
+            chat_id=AUTH_CHANNEL, 
+            text=message.text
+        )
+    else:
+        await client.send_message(
+            chat_id=AUTH_CHANNEL,
+            text=f"⚡ {message.text}"
+        )
