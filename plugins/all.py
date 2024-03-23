4fbdb991707c77246ec1cf7b30fc393aeedffdef
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-23 14:50:04 +0530

    Update all.py

--- plugins/all.py
+++ plugins/all.py
@@ -1,20 +1,17 @@
-from pyrogram import Client
-from pyrogram import filters
-
+from pyrogram import Client, filters
 
 # Define the source group and target channel IDs
 source_group_id = -1001932992146  # Replace with the source group ID
 target_channel_id = -1001726137386  # Replace with the target channel ID
 
 # Define the function to forward messages from bots in the source group to the target channel
-@Client.on_message(filters.chat(source_group_id) & filters.bot)
+@Client.on_message(filters.chat(source_group_id) & filters.text & filters.bot)
 async def forward_bots(client, message):
+    content = message.text
     try:
-        await client.forward_messages(
+        await client.send_message(
             chat_id=target_channel_id, 
-            from_chat_id=source_group_id,
-            message_ids=message.message_id
+            text=content
         )
     except Exception as e:
         print(f"An error occurred while forwarding message: {e}")
-
